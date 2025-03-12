b1b6a5faa6bf79f986eee6744a69b71e59ee0be5
Youssefgamalsa <yj0820996@gmail.com>
2025-03-12 13:46:33 +0200

    second-commit2

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -113,18 +113,7 @@ export default function Sidebar() {
           {" "}
           Number of Reservations{" "}
         </h1>
-        <BarChart
-          xAxis={[
-            { scaleType: "band", data: ["group A", "group B", "group C"] },
-          ]}
-          series={[
-            { data: [4, 3, 5] },
-            { data: [1, 6, 3] },
-            { data: [2, 5, 6] },
-          ]}
-          width={900}
-          height={500}
-        />
+       
 
         <table className="table table-hover shadow w-100">
           <thead>
